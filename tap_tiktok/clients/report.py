27b7b26b8cd87937f968b811d4fc906eee5ab1f5
Andrio Frizon <andrio@nekt.ai>
2024-11-05 17:30:04 -0300

    feat: Extract pagination to its own class

--- tap_tiktok/clients/report.py
+++ tap_tiktok/clients/report.py
@@ -1,22 +1,17 @@
 import abc
-import datetime
 import json
 import typing as t
 from functools import cached_property
 from typing import Any
-from urllib.parse import parse_qs, urlparse
 
 import pendulum
-import requests
 from singer_sdk import metrics
 from singer_sdk import typing as th
-from singer_sdk.helpers.jsonpath import extract_jsonpath
 from singer_sdk.streams.core import Context
 
+from tap_tiktok.pagination import DailyReportPaginator
+
 from .base import TikTokStream
-
-# if t.TYPE_CHECKING:
-
 
 DATE_FORMAT = "%Y-%m-%d"
 STEP_NUM_DAYS = 30
@@ -64,71 +59,28 @@ class TikTokReportStream(TikTokStream, metaclass=abc.ABCMeta):
     records_jsonpath = "$.data.list[*]"
     next_page_token_jsonpath = "$.page_info.page"
 
-    def post_process(self, row: dict, context: dict | None = None) -> dict | None:
-        return {**row["dimensions"], **row["metrics"]}
+    def _get_start_datetime(self, context: Context | None) -> pendulum.DateTime:
+        start_date: pendulum.DateTime = self.get_starting_timestamp(context)
+        lookback_window = self.config["lookback"]
+        if lookback_window > 0:
+            start_date = max(
+                min(start_date, pendulum.now(tz=start_date.tzinfo).subtract(days=lookback_window)),
+                pendulum.parse(self.config["start_date"]),
+            )
+        return start_date
 
-    # TODO: Move this to a paginator class
-    @staticmethod
-    def _get_page_info(json_path, json):
-        page_matches = extract_jsonpath(json_path, json)
-        return next(iter(page_matches), None)
-
-    # TODO: Move this to a paginator class
-    def get_next_page_token(self, response: requests.Response, previous_token: Any | None) -> Any | None:
-        """Return a token for identifying next page or None if no more pages."""
-        current_page = self._get_page_info("$.data.page_info.page", response.json()) or 0
-        total_pages = self._get_page_info("$.data.page_info.total_page", response.json()) or 0
-        start_date = datetime.datetime.strptime(
-            parse_qs(urlparse(response.request.url).query)["start_date"][0], DATE_FORMAT
-        )
-        yesterday = datetime.datetime.now(tz=start_date.tzinfo) - datetime.timedelta(days=1)
-        end_date = datetime.datetime.strptime(
-            parse_qs(urlparse(response.request.url).query)["end_date"][0], DATE_FORMAT
-        )
-        if current_page < total_pages:
-            return {"page": current_page + 1, "start_date": previous_token["start_date"] if previous_token else None}
-        elif end_date.date() < yesterday.date():
-            return {
-                "page": 1,
-                "start_date": min(end_date + datetime.timedelta(days=1), yesterday).strftime(DATE_FORMAT),
-            }
-        return None
+    def get_new_paginator(self):
+        start_date = self._get_start_datetime(context=None)
+        return DailyReportPaginator(start_date)
 
     def get_url_params(self, context: dict | None, next_page_token: Any | None) -> dict[str, Any]:
-        """Return a dictionary of values to be used in URL parameterization."""
-        if isinstance(next_page_token, dict) and next_page_token["start_date"] is not None:
-            start_date = datetime.datetime.strptime(next_page_token["start_date"], DATE_FORMAT)
-        else:
-            start_date = self.get_starting_timestamp(context)
-
-            # picking up where we left off on the last run (or first run), adjust for lookback if set
-            lookback_window = self.config["lookback"]
-            if lookback_window > 0:
-                # if lookback is configured, we want to refetch data for the entire lookback window
-                # (or as far back as the configured start date, whichever is the most recent date)
-                start_date = max(
-                    min(
-                        start_date,
-                        datetime.datetime.now(tz=start_date.tzinfo) - datetime.timedelta(days=lookback_window),
-                    ),
-                    pendulum.parse(self.config["start_date"]),
-                )
-
-        yesterday = datetime.datetime.now(tz=start_date.tzinfo) - datetime.timedelta(days=1)
-        end_date = min(start_date + datetime.timedelta(days=STEP_NUM_DAYS), yesterday)
-        self.logger.info(
-            f"Preparing request for {start_date.strftime(DATE_FORMAT)} - {end_date.strftime(DATE_FORMAT)}."
-        )
         params: dict = {
-            "page_size": 1000,
             "advertiser_id": self.config.get("advertiser_id"),
             "service_type": "AUCTION",
             "report_type": self.report_type,
             "data_level": self.data_level,
             "dimensions": json.dumps(self.dimensions),
             "metrics": json.dumps(self.metrics_keys),
-            "start_date": start_date.strftime(DATE_FORMAT),
-            "end_date": end_date.strftime(DATE_FORMAT),
             "filtering": json.dumps(
                 [
                     {
@@ -146,22 +98,15 @@ class TikTokReportStream(TikTokStream, metaclass=abc.ABCMeta):
                 ]
             ),
         }
-        if next_page_token:
-            params["page"] = next_page_token["page"]
+        params = {
+            **params,
+            **next_page_token,
+        }
+        self.logger.info(f"Preparing request for {next_page_token['start_date']} - {next_page_token['end_date']}.")
 
         return params
 
     def request_records(self, context: Context | None) -> t.Iterable[dict]:
-        """Request records from REST endpoint(s), returning response records.
-
-        If pagination is detected, pages will be recursed automatically.
-
-        Args:
-            context: Stream partition or context dictionary.
-
-        Yields:
-            An item for every record in the response.
-        """
         paginator = self.get_new_paginator()
         decorated_request = self.request_decorator(self._request)
         pages = 0
@@ -192,3 +137,6 @@ class TikTokReportStream(TikTokStream, metaclass=abc.ABCMeta):
                 pages += 1
 
                 paginator.advance(resp)
+
+    def post_process(self, row: dict, context: dict | None = None) -> dict | None:
+        return {**row["dimensions"], **row["metrics"]}
---
+++ tap_tiktok/pagination.py
@@ -0,0 +1,55 @@
+from urllib.parse import parse_qs, urlparse
+
+import pendulum
+from requests import Response
+from singer_sdk.pagination import BaseAPIPaginator
+
+PAGE_SIZE = 1000
+STEP_NUM_DAYS = 30
+
+
+class DailyReportPaginator(BaseAPIPaginator):
+    def __init__(self, start_date: pendulum.DateTime):
+        yesterday = pendulum.now().subtract(days=1)
+        end_date = min(start_date.add(days=STEP_NUM_DAYS), yesterday)
+
+        start_value = {
+            "page_size": PAGE_SIZE,
+            "page": 1,
+            "start_date": start_date.to_date_string(),
+            "end_date": end_date.to_date_string(),
+        }
+        super().__init__(start_value)
+
+    def has_more(self, response: Response) -> bool:
+        page_info = response.json().get("data", {}).get("page_info", {})
+        current_page = page_info.get("page", 0)
+        total_pages = page_info.get("total_page", 0)
+        if current_page < total_pages:
+            return True
+        start_date = pendulum.parse(self.current_value["start_date"])
+        yesterday = pendulum.now().subtract(days=1)
+        end_date = min(start_date.add(days=STEP_NUM_DAYS), yesterday)
+        if end_date.date() < yesterday.date():
+            return True
+        return False
+
+    def get_next(self, response) -> dict[str, str]:
+        page_info = response.json().get("data", {}).get("page_info", {})
+        current_page = page_info.get("page", 0)
+        total_pages = page_info.get("total_page", 0)
+        if current_page < total_pages:
+            return {
+                **self.current_value,
+                "page": self.current_value["page"] + 1,
+            }
+        start_date = pendulum.parse(self.current_value["end_date"]).add(days=1)
+        yesterday = pendulum.now().subtract(days=1)
+        start_date = min(start_date, yesterday)
+        end_date = min(start_date.add(days=STEP_NUM_DAYS), yesterday)
+        return {
+            "page_size": PAGE_SIZE,
+            "page": 1,
+            "start_date": start_date.to_date_string(),
+            "end_date": end_date.to_date_string(),
+        }
